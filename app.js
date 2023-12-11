0c80d67abb4d11970b1f16584b45cdb21a51d8d7
Darowl <darowl.dev@gmail.com>
2023-12-11 11:49:39 -0600

    solution remake

--- app.js
+++ app.js
@@ -1,57 +1,36 @@
-const btnRate = document.querySelectorAll(".button-rate")
-const btnSub = document.querySelector('#buttonSubmit')
-const body = document.getElementById('body')
+const buttonRating = document.querySelectorAll(".rating-btn")
+const btnSubmit = document.getElementById('buttonSubmit')
+const text = document.getElementById('thanks-rating')
 
 
-// Buttons Rate 
-
-let getNum = "0"
-
-let getNumber = (e) => {
-  let num = e.target.innerText
-  return getNum = num
-}
+let ratingNumber;
 
 
-
-const btnColor = (e) => {
-  btnRate.forEach(btn => {
-    btn.className = "p-4 rounded-full button-rate"
+buttonRating.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if(btn.id == "null") {
+      for(let i = 0; i < buttonRating.length; i++) {
+        buttonRating[i].id = "null";
+      }
+      btn.id = "selected";
+      ratingNumber = parseFloat(btn.textContent);
+    }
   })
-
-    let focus = "p-4 rounded-full button-rate btn-focus"
-    return e.target.className = focus
-}
-
-btnRate.forEach(btn => {
-
-  btn.addEventListener("click", getNumber)
-  btn.addEventListener('click', btnColor)
-  
 });
 
 
-// Button submit
+btnSubmit.addEventListener('click', () => {
+  if (ratingNumber > 0 && ratingNumber <= 5) {
+    document.querySelector('.card-submit').classList.toggle('hide')
 
-let addCard = () => {
-  let numSub = `<span>${getNum}</span>`;
+    console.log(document.querySelector('.card-submit').className);
 
-  const html = `<section class="w-4/5 max-w-sm">
-<div class="card card-tanks flex flex-col p-4 rounded-xl items-center">
-  <div class="flex justify-center my-5 ">
-    <img  src="./images/illustration-thank-you.svg" alt="Tank you img">
-  </div>
- 
-  <span class="card-selection my-4 rounded-full w-fit px-3 py-1.5">You selected ${numSub} out of 5</span>
+    document.querySelector('.card-thanks').classList.toggle('hide')
 
-  <h1 class="card-header py-3 text-xl font-bold">Thank you!</h1>
-  <p class="card-text text-[15px] py-2"> We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
-</div>
-</section>`;
+    text.textContent = `You selected ${ratingNumber} out of 5`
+  }
+})
 
-  body.innerHTML = html
-}
 
-btnSub.addEventListener("click", addCard)
 
 
